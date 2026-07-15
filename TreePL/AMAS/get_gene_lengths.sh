5ed90db84de7a7a87a6608ac287356ff7169ae99
Julia McClafferty <julia.mcclafferty@univie.ac.at>
2026-07-15 16:44:24 +0200

    Create get_gene_lengths.sh

---
+++ TreePL/AMAS/get_gene_lengths.sh
@@ -0,0 +1,7 @@
+# you can run this locally or as one-liners in the terminal 
+
+## create new file with just length of alignment 
+awk 'NR>1 {print $1 "\t" $3}' summary.txt > genelengths.txt
+
+## get average gene lengths for --genelength flag on ASTRAL
+awk '{sum += $2; n++} END {if (n>0) printf "%.6f\n", sum/n; else print 0}' genelengths.txt
